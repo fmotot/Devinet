e61e7b11bae116873eda53c3aa7394600a11a18f
de Lajarte <matmat2983@gmail.com>
2020-08-11 11:11:31 +0200

    Auto stash before merge of "master" and "origin/master"
    Play activity

---
+++ app/src/main/java/fr/eni/devinet/activity/PlayActivity.java
@@ -0,0 +1,16 @@
+package fr.eni.devinet.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fr.eni.devinet.R;
+
+public class PlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+    }
+}
