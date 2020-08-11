6a5f72ddb89cbf647e036a4405aaed7bc7eb5289
de Lajarte <matmat2983@gmail.com>
2020-08-11 12:22:36 +0200

    avancement activity play

--- app/src/main/java/fr/eni/devinet/activity/PlayActivity.java
+++ app/src/main/java/fr/eni/devinet/activity/PlayActivity.java
@@ -2,15 +2,42 @@ package fr.eni.devinet.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import fr.eni.devinet.R;
 
-public class PlayActivity extends AppCompatActivity {
+public class PlayActivity extends MenuActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+
+        // modification de l'image dynamiquement
+        ImageView iv_img_modif = findViewById(R.id.iv_img);
+        iv_img_modif.setImageResource(R.drawable.avocat);
+
+        // Zone de proposition
+
+
+
+        /*
+        LinearLayout layoutPlay = findViewById(R.id.play_activity);
+        // ajout d'une liste view
+        TextView textView = new TextView(this);
+        textView.setText("I am added dynamically to the view");
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+        );
+        textView.setLayoutParams(params);
+        layoutPlay.addView(textView);
+
+        */
     }
 }
