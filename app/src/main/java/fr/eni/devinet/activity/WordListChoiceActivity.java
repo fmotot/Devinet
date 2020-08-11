af88ec9a7bb081b5ff673d5f4b1c99e62123bebc
Frédérick <fmotot@gmail.com>
2020-08-11 11:30:43 +0200

    Modification sur les intent depuis Level et WordList Activity

--- app/src/main/java/fr/eni/devinet/activity/WordListChoiceActivity.java
+++ app/src/main/java/fr/eni/devinet/activity/WordListChoiceActivity.java
@@ -1,10 +1,10 @@
 package fr.eni.devinet.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -20,7 +20,9 @@ import fr.eni.devinet.view_model.WordListViewModel;
 
 public class WordListChoiceActivity extends MenuActivity {
 
+    private static final String WORDLIST_ID = "wordListId";
     private ListView lvWordList;
+    private WordListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,14 +32,14 @@ public class WordListChoiceActivity extends MenuActivity {
         WordListViewModel vm = ViewModelProviders.of(this).get(WordListViewModel.class);
         lvWordList = findViewById(R.id.lv_word_list);
 
-        int levelId = getIntent().getIntExtra(LevelChoiceActivity.WORDLIST_ID, 1);
+        int levelId = getIntent().getIntExtra(LevelChoiceActivity.LEVEL_ID, 1);
 
         LiveData<List<WordListWithProgress>> wordLists = vm.getWordListWithProgress(levelId);
 
         wordLists.observe(this, new Observer<List<WordListWithProgress>>() {
             @Override
             public void onChanged(List<WordListWithProgress> wordListWithProgresses) {
-                WordListAdapter adapter = new WordListAdapter(WordListChoiceActivity.this, R.layout.style_word_list_list, wordListWithProgresses);
+                adapter = new WordListAdapter(WordListChoiceActivity.this, R.layout.style_word_list_list, wordListWithProgresses);
                 lvWordList.setAdapter(adapter);
             }
         });
@@ -46,6 +48,11 @@ public class WordListChoiceActivity extends MenuActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Toast.makeText(WordListChoiceActivity.this, "Click sur liste de mot sur position : " + i, Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(WordListChoiceActivity.this, WordListChoiceActivity.class);
+                intent.putExtra(WORDLIST_ID, adapter.getItem(i).getWordList().getId());
+
+                startActivity(intent);
             }
         });
     }
