60218656613338ec08aff16e27f5dc6f9a7880ff
Frédérick <fmotot@gmail.com>
2020-08-12 11:54:53 +0200

    PlayActivity images, clic sur lettre et gomme OK
    Ajout d'une icone pour le background des lettres
    Ajout d'une couleur pour les lettres sélectionnées

--- app/src/main/java/fr/eni/devinet/activity/PlayActivity.java
+++ app/src/main/java/fr/eni/devinet/activity/PlayActivity.java
@@ -1,6 +1,7 @@
 package fr.eni.devinet.activity;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.TypedValue;
@@ -8,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
@@ -30,19 +30,26 @@ import fr.eni.devinet.view_model.PlayViewModel;
 
 public class PlayActivity extends MenuActivity {
 
+    private static final int TV_LETTER_ID = 1;
+    private static final int IV_BACKGROUND_ID = 0;
+
     private int numWord = 0;
     private Word currentWord;
     private Context context;
     private MutableLiveData<String> proposal = new MutableLiveData<>();
+
+    private float pixelFactor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
 
-        proposal.setValue("");
         context = this;
+        pixelFactor = this.getResources().getDisplayMetrics().density;
         int wordListId = getIntent().getIntExtra(WordListChoiceActivity.WORDLIST_ID, 1);
+        proposal.setValue("");
 
         PlayViewModel vm = ViewModelProviders.of(this).get(PlayViewModel.class);
 
@@ -66,15 +73,7 @@ public class PlayActivity extends MenuActivity {
                 TextView tvTest = findViewById(R.id.tv_test);
                 tvTest.setText(currentWord.getWord());
 
-
-                /**
-                 * Gestion de la proposition du mot
-                 */
                 setProposalView();
-
-                /**
-                 * Affichage des lettres du mot
-                 */
                 setLetters();
             }
         });
@@ -82,16 +81,23 @@ public class PlayActivity extends MenuActivity {
         proposal.observe(this, new Observer<String>() {
             @Override
             public void onChanged(String s) {
-                Toast.makeText(context, "Mot : " + s, Toast.LENGTH_LONG).show();
                 setProposalView();
             }
         });
     }
 
-    private void setProposal(String s){
+    /**
+     * Ajoute une lettre au mot proposé en cours
+     *
+     * @param s
+     */
+    private void setProposal(String s) {
         proposal.setValue(proposal.getValue() + s);
     }
 
+    /**
+     * Affichage des lettres du mot
+     */
     private void setLetters() {
 
         // suffle des lettres du mot
@@ -102,11 +108,12 @@ public class PlayActivity extends MenuActivity {
 
 
         // calcul du nombre de ligne & lettres / ligne
-        // TODO voir pour un calcul plus homogène
+        // TODO voir pour un affichage sur plusieurs ligne
 
         int nbLigne;
         int maxLetters;
 
+        /*
         switch (nbLetters) {
             case 3:
                 nbLigne = 1;
@@ -125,6 +132,7 @@ public class PlayActivity extends MenuActivity {
                 nbLigne = 3;
                 maxLetters = nbLetters / nbLigne;
         }
+        */
 
         // créer les layout et les remplir des lettres
         // ajouter action onClick sur "chaque lettre"
@@ -151,32 +159,21 @@ public class PlayActivity extends MenuActivity {
         View.OnClickListener onClick = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(context, "Click sur la lettre : " + ((TextView)view).getText(), Toast.LENGTH_LONG).show();
-                setProposal(((TextView)view).getText().toString());
-                ((TextView) view).setTextColor(getColor(R.color.yellow));
+                if (!(Boolean) view.getTag()) {
+                    TextView tv = (TextView) ((ViewGroup) view).getChildAt(TV_LETTER_ID);
+                    setProposal(tv.getText().toString());
+                    ImageView iv = (ImageView) ((ViewGroup) view).getChildAt(IV_BACKGROUND_ID);
+                    iv.setImageTintList(ColorStateList.valueOf(getColor(R.color.orange)));
+                    // lettre utilisée
+                    view.setTag(true);
+                }
             }
         };
 
-
         for (String letter : letters) {
 
-            // Création du textView de la lettre
-            TextView tv = new TextView(context);
-            tv.setText(letter);
-            tv.setTextColor(getColor(R.color.green));
-            tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 25);
-            tv.setId(View.generateViewId());
-
-            // Ajout des contraintes sur le contenu
-            ConstraintLayout.LayoutParams wrap = new ConstraintLayout.LayoutParams(
-                    ConstraintLayout.LayoutParams.WRAP_CONTENT,
-                    ConstraintLayout.LayoutParams.WRAP_CONTENT
-            );
-            wrap.setMargins(50, 10, 50, 10);
-
-            tv.setLayoutParams(wrap);
-
             // ! Bien ajouter les view avant de CREER les constraints de placement (avant le clone)
+            View tv = createViewShuffle(letter);
             line.addView(tv);
             tv.setOnClickListener(onClick);
 
@@ -196,6 +193,63 @@ public class PlayActivity extends MenuActivity {
         }
     }
 
+    /**
+     * Ajoute une vue contenant la lettre avec une image de fond
+     *
+     * @param letter
+     * @return
+     */
+    private View createViewShuffle(String letter) {
+
+        // Création d'un layout pour contenir le fond et la lettre
+        ConstraintLayout clLetter = new ConstraintLayout(context);
+        clLetter.setId(View.generateViewId());
+        clLetter.setTag(false);
+        ConstraintLayout.LayoutParams wrap = new ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT
+        );
+        wrap.setMargins(50, 10, 50, 10);
+        clLetter.setLayoutParams(wrap);
+
+        // ajout de l'image
+        ImageView ivBackground = new ImageView(context);
+        ivBackground.setId(View.generateViewId());
+        ivBackground.setImageDrawable(getDrawable(R.drawable.ic_paper_letter2));
+        ivBackground.setRotation(90);
+        ivBackground.setImageTintList(ColorStateList.valueOf(getColor(R.color.green)));
+        ivBackground.setLayoutParams(new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT));
+        ivBackground.getLayoutParams().height = (int) (60 * pixelFactor);
+        ivBackground.getLayoutParams().width = (int) (40 * pixelFactor);
+        clLetter.addView(ivBackground);
+
+        // Création du textView de la lettre
+        TextView tv = new TextView(context);
+        tv.setId(View.generateViewId());
+        tv.setText(letter);
+        tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 25);
+        wrap = new ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT
+        );
+        tv.setLayoutParams(wrap);
+        clLetter.addView(tv);
+
+        // Contrainte de la lettre dans le layout
+        ConstraintSet cs = new ConstraintSet();
+        cs.clone(clLetter);
+        cs.connect(tv.getId(), ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT);
+        cs.connect(tv.getId(), ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT);
+        cs.connect(tv.getId(), ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP);
+        cs.connect(tv.getId(), ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM);
+        cs.applyTo(clLetter);
+
+        return clLetter;
+    }
+
+    /**
+     * Gestion de la proposition du mot
+     */
     private void setProposalView() {
         ConstraintLayout clLineProposal = findViewById(R.id.cl_line_proposal);
         ((ViewGroup) clLineProposal).removeAllViews();
@@ -203,7 +257,6 @@ public class PlayActivity extends MenuActivity {
 
 
         List<String> prop = new ArrayList<>();
-        // TODO pour test, à modifier
         // TODO voir pour créer un observer sur proposal
         for (char c : Objects.requireNonNull(proposal.getValue()).toCharArray()) {
             prop.add(String.valueOf(c));
@@ -255,7 +308,18 @@ public class PlayActivity extends MenuActivity {
         // Réinitialiser la proposition
         proposal.setValue("");
 
-        // Réinitialiser la couleur d'origine
-
+        // Réinitialiser la couleur d'origine et l'utilisation de la lettre
+        ViewGroup clLetters = findViewById(R.id.cl_letters);
+        // pour toute les lignes de lettre
+        for (int indexLine = 0; indexLine < clLetters.getChildCount(); indexLine++){
+            ViewGroup line = (ViewGroup)clLetters.getChildAt(indexLine);
+            //pour chaque lettre (layout) dans la ligne
+            for(int indexChild = 0; indexChild < line.getChildCount(); indexChild++){
+                View letterContainer = line.getChildAt(indexChild);
+                letterContainer.setTag(false);
+                ImageView iv = (ImageView) ((ViewGroup) letterContainer).getChildAt(IV_BACKGROUND_ID);
+                iv.setImageTintList(ColorStateList.valueOf(getColor(R.color.green)));
+            }
+        }
     }
 }
